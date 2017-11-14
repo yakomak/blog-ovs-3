cc9375a137d9a881f4f62e268e18e499ed87fa5d
yakomak <yakomak137@gmail.com>
2017-11-14 16:12:42 +0300

    почистили

--- server2.js
+++
@@ -1,9 +0,0 @@
-var http = require('http')
-var statics = require('node-static')
-var file = new statics.Server('.')
-
-http.createServer(function (req, res) {
-  file.serve(req, res)
-}).listen(8888)
-
-console.log('Server running on port 8888')
